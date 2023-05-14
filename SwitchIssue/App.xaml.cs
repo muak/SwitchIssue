aa0f4f9669493c25b1674d92ad2c3e81b5f99336
Satoshi Nakamura <kamuxxx@hotmail.com>
2023-05-14 16:57:44 +0900

    First commit

---
+++ SwitchIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SwitchIssue;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ SwitchIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwitchIssue;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
